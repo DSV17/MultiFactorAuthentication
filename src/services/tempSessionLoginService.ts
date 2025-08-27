ee1a9fcf47929f2e75b6c93d34b447298f2bfe5f
DSV17 <dnlout@gmail.com>
2025-08-27 17:02:14 -0300

    feature: Adicionando funções para sessions

---
+++ src/services/tempSessionLoginService.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import crypto from 'crypto';
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+export const createTempSession = async (userId: string, expiresInMinutes: number = 5) => {
+  const token = crypto.randomBytes(32).toString('hex');
+  const hashedToken = await bcrypt.hash(token, 10);
+  const expiresAt = new Date(Date.now() + expiresInMinutes * 60 * 1000);
+
+  return prisma.tempLoginSession.create({
+    data: {
+      userId,
+      hashedToken,
+      expiresAt,
+    },
+  });
+};
+
+export const validateTempSession = async (userId: string, token: string) => {
+  const session = await prisma.tempLoginSession.findUnique({
+    where: { userId }
+  });
+
+  if(!session) return false;
+  const isSession = await bcrypt.compare(token, session.hashedToken);
+
+  return isSession;
+};
